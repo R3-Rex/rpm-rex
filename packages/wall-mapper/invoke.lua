7eaad9afc3bb3f9217f14ba26b987d4dd1796fab
RexGameStudio <101283720+RexGameStudio@users.noreply.github.com>
2022-06-11 20:09:00 -0600

    4.7b w/ version changed

--- packages/wall-mapper/invoke.lua
+++ packages/wall-mapper/invoke.lua
@@ -58,7 +58,7 @@ function ScanUpRow()
     end
 end
 
-cPrint("Starting Drone v4.6b", colors.lime)
+cPrint("Starting Drone v4.7b", colors.lime)
 os.sleep(1)
 cPrint(dividerDashes)
 cPrint("Loading Apis")
