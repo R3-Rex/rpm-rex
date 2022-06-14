94441c8009bfaf7f6026507b4418d9b635b1fb22
RexGameStudio <101283720+RexGameStudio@users.noreply.github.com>
2022-06-13 19:26:02 -0600

    Fixed Again

--- packages/wall-mapper/util/turtleBuild.lua
+++ packages/wall-mapper/util/turtleBuild.lua
@@ -8,14 +8,9 @@ function buildDown()
             built = true
         else
             if (turtle.detectDown()) then
-                if builtDone() then
-                    built = true
-                end
-                if not built then
-                    if not turtle.digDown() then
-                        commApi.SendRequest("STATUS Indestructable Down")
-                        os.sleep(1)
-                    end
+                if not turtle.digDown() then
+                    commApi.SendRequest("STATUS Indestructable Down")
+                    os.sleep(1)
                 end
             end
         end
